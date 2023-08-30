1b884bb9daaddb03b3073fae212c21fb6912caea
Cody Ko In A Box <codykoinabox@gmail.com>
2023-08-29 21:29:13 -0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -19,3 +19,37 @@ DELETE FROM Estado WHERE nome = "Parana";
 
 
 SELECT * FROM Estado;
+
+
+INSERT INTO Estado(nome) VALUES
+("Amazonas"),
+("Rio Grande do Sul"),
+("Pernambuco"),
+("Mato Grosso"),
+("Mato Grosso do Sul"),
+("Espirito Santo"),
+("Goias"),
+("Distrito Federal"),
+("Piaui"),
+("Acre"),
+("Rondonia"),
+("Roraima"),
+("Amapa"),
+("Para"),
+("Tocantins"),
+("Bahia"),
+("Ceara"),
+("Paraiba");
+
+
+DESC Cidade;
+
+
+INSERT INTO Cidade(nome, estado_id) VALUES
+("Sao Paulo", 9),
+("Rio De Janeiro", 10),
+("Goiania", 23),
+("Curitiba", 8);
+
+
+SELECT * FROM Cidade;
